a71b300d6691ec54986fe1d9d2531b4fc995f095
Mac-Mini-2 <Mac-Mini-2@mac-mini-2.odc.localdomain>
2016-02-01 15:48:55 +0530

    Initial Commit

---
+++ Weather App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather App
+//
+//  Created by Mac-Mini-2 on 01/02/16.
+//  Copyright © 2016 Mac-Mini-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weather App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Weather App
+//
+//  Created by Mac-Mini-2 on 01/02/16.
+//  Copyright © 2016 Mac-Mini-2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
